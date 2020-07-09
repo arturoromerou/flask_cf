7e1341fa6263d6ab2c3d3dbd3d6f86f4f4c4d4a8
arturoromeroum <ajruwork@gmail.com>
2020-07-08 20:37:37 -0500

    se mejora el proyecto con sesiones

---
+++ Project/forms.py
@@ -0,0 +1,38 @@
+from wtforms import Form # formularios de flask
+from wtforms import StringField, TextField, PasswordField # formato para nuestros inputs
+from wtforms.fields.html5 import EmailField # formato de correo electronico para input
+from wtforms import HiddenField # oculta los campos que deseemos
+from wtforms import validators # esto valida los campos que deseemos
+
+def length_honeypot(form, field):
+    if len(field.data) > 0:
+        raise validators.ValidationError('el campo debe estar vacio')
+
+class CommentForm(Form):
+    username = StringField('username',
+                        [ 
+                            validators.Required(message='el username es requerido'),
+                            validators.length(min=4, max=25, message='ingresa un username valido!.')
+                        ]
+                        )
+    email = EmailField('correo electronico',
+                    [
+                        validators.Required(message='el username es requerido'),
+                        validators.Email(message='ingresa un email valido')
+                    ]
+                    )
+    comment = TextField('comentario')
+    honeypot = HiddenField('', [length_honeypot])
+
+class LoginForm(Form):
+    username = StringField('Username',
+                        [ 
+                            validators.Required(message='el username es requerido'),
+                            validators.length(min=4, max=25, message='ingresa un username valido!.')
+                        ]
+                        )
+    password = PasswordField('Password', 
+                            [
+                                validators.Required(message='El password es requerido')
+                            ]
+                            )
--- Project/main.py
+++ Project/main.py
@@ -1,12 +1,61 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask # importa flask
+from flask import render_template # renderiza los templates
+from flask import request #
+from flask import make_response
+from flask import session
+
+from flask import url_for
+from flask import redirect
+
+from flask_wtf.csrf import CSRFProtect # evita los ataques csrf con un secret key
+import forms
 
 app = Flask(__name__)
+app.secret_key = 'Ar2098'
+csrf = CSRFProtect(app)
 
 @app.route('/')
 def index():
-    title = "Curso Flask"
+    if 'username' in session:
+        username = session['username']
+        print(username)
+    title = 'Index'
     return render_template('index.html', title = title)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    title = 'Login'
+    login_form = forms.LoginForm(request.form)
+    if request.method == 'POST' and login_form.validate():
+        session['username'] = login_form.username.data
+
+    return render_template('login.html', title = title, form = login_form)
+
+@app.route('/logout')
+def logout():
+    if 'username' in session:
+        session.pop('username')
+    return redirect(url_for('login'))
+
+@app.route('/cookie')
+def method_name():
+    title = 'Cookies'
+    response = make_response(render_template('cookie.html', title = title))
+    response.set_cookie('custom_cookie', 'Arturo')
+    return response
+
+@app.route('/comment', methods = ['GET', 'POST'])
+def comment():
+    comment_form = forms.CommentForm(request.form)
+    if request.method == 'POST' and comment_form.validate():
+        print(comment_form.username.data)
+        print(comment_form.email.data)
+        print(comment_form.comment.data)
+    else:
+        print('error en el formulario')
+
+    title = "Comment"
+    return render_template('comment.html', title = title, form = comment_form)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
